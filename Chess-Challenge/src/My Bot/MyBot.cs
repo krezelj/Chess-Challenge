17adb82ce2a9223a1a8c516ff58c828dfdd84752
Jasiek <jasiekk9@gmail.com>
2023-07-30 14:31:42 +0200

    Implement basic DFS Search

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,59 @@
 ﻿using ChessChallenge.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class MyBot : IChessBot
 {
+    //class MoveNode
+    //{
+    //    public Move move;
+    //    public float evaluation;
+    //    public List<MoveNode> nextMoves;
+    //}
+
+
+
     public Move Think(Board board, Timer timer)
     {
+        int depth = 4;
+        int movesEvaluated = 0; // #DEBUG
+        Stack<Move> moveStack = new Stack<Move>(board.GetLegalMoves());
+        Stack<Move> moveHistoryStack = new Stack<Move>();
+        Stack<int> sizeStack = new Stack<int>(new int[] { moveStack.Count });
+        while (moveStack.Count > 0)
+        {
+            while (sizeStack.Peek() == 0)
+            {
+                depth++;
+                sizeStack.Pop();
+                board.UndoMove(moveHistoryStack.Pop());
+            }
+
+            movesEvaluated++; // #DEBUG
+            Move currentMove = moveStack.Pop();
+            sizeStack.Push(sizeStack.Pop() - 1); // decrement value of the last element
+            if (depth == 0)
+                //if depth 0, apply move, evaluate, undo move and push the value up
+                continue;
+
+            board.MakeMove(currentMove);
+            moveHistoryStack.Push(currentMove);
+
+            depth--;
+            Move[] newMoves = board.GetLegalMoves();
+            sizeStack.Push(newMoves.Length);
+            foreach (var move in newMoves) 
+                moveStack.Push(move);
+        }
+        while (moveHistoryStack.Count > 0)
+        {
+            board.UndoMove(moveHistoryStack.Pop());
+        }
+        Console.WriteLine(moveHistoryStack.Count.ToString());
+        Console.WriteLine($"Moves Evaluated: {movesEvaluated}"); // #DEBUG
         Move[] moves = board.GetLegalMoves();
         return moves[0];
+
     }
 }
