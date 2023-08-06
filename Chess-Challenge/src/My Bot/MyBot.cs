13943cd32f373a669131d5dec40b655ed26b81af
Jasiek <jasiekk9@gmail.com>
2023-08-06 07:01:20 +0200

    Add ELO difference

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class MyBot : IChessBot
 {
-    //                      None    Pawn    Knight  Bishop  Rook    Queen   King
+    // None    Pawn    Knight  Bishop  Rook    Queen   King
     int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
 
     public Move Think(Board board, Timer timer)
